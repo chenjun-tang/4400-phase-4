50612717018026a9f78f0e4fbf0da40920d13953
chenjun-tang <tangchenjun11@gmail.com>
2020-11-12 00:20:30 -0500

    Modified home page

--- app.py
+++ app.py
@@ -3,10 +3,22 @@ import os
 
 app = Flask(__name__, template_folder="templates", static_folder="static")
 
-@app.route("/")
+@app.route("/", methods=['GET', 'POST'])
 def index():
     return render_template("index.html")
 
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    return render_template('register.html')
+
+@app.route("/home")
+def home():
+    user_type = "Student"
+    user_type = "Tester"
+    user_type = "Admin"
+    user_type = "Lab Technician"
+    user_type = "Lab Technician/Tester"
+    return render_template("home.html", user_type = user_type)
 
 if __name__ == '__main__':
     app.run(debug=True)
