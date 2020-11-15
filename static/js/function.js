4912063a3feec9f0b42bb1959dd02c4780fe6da7
sun199609 <sunzhimin1996@126.com>
2020-11-14 19:29:23 -0600

    passwd length

--- static/js/function.js
+++ static/js/function.js
@@ -3,8 +3,13 @@ function validate(){
     var pwd2 = document.getElementById("conf_pwd").value;
     <!-- compare two passwds -->
     if(pwd1 == pwd2) {
-        document.getElementById("tips").innerHTML="<font color='green'>Passwords are consistent.</font>";
-        document.getElementById("mySubmit").disabled = false;
+        if(pwd1.length < 8){
+            document.getElementById("tips").innerHTML="<font color='red'>Password must be at least 8 characters in size.</font>";
+            document.getElementById("mySubmit").disabled = true;
+        }else{
+            document.getElementById("tips").innerHTML="<font color='green'>Passwords are consistent.</font>";
+            document.getElementById("mySubmit").disabled = false;
+        } 
     }
     else {
         document.getElementById("tips").innerHTML="<font color='red'>Passwords are not consistent.</font>";
